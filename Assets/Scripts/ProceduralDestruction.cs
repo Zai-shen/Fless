4844aa4727152146bd832902d087e95fd710c870
Miran Jank <e1526438@mail.student.tuwien.ac.at>
2020-09-18 15:10:16 +0200

    Procedural spawning and destruction of lines (platforms[tile,trigger,tile]). Closes #4
    1h30m

---
+++ Assets/Scripts/ProceduralDestruction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralDestruction : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("DestroyTrigger"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
